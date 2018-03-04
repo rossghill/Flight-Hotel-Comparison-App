4919317284c6043ac3d38b22c38db1fb5b066a16
Eric Carles <carles.eric@wanadoo.fr>
2018-03-04 13:15:03 +0000

    Configuration of  server.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,7 +2,7 @@ config = {
   entry: __dirname + "/src/app.js",
   output: {
     filename: "bundle.js",
-    path: __dirname + "/build"
+    path: __dirname + "/client/build/public"
   },
   devtool: 'source-map'
 };
---
+++ server.js
@@ -0,0 +1,25 @@
+const express           = require('express');
+const server            = express();
+//const router            = require('./server/controllers/index.js');
+const path              = require('path');
+//const MongoClient       = require('mongodb').MongoClient;
+//const ObjectID          = require('mongodb').ObjectID;
+const bodyParser        = require('body-parser');
+
+
+
+//server.use(router);
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
+server.use(express.static(path.join(__dirname, 'client/build/public')));
+
+server.get("/", function(req, res){
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  //res.json({"HELLO"});
+})
+
+
+
+server.listen(3000, function () {
+  console.log('FlightR running on port ' + this.address().port);
+});
