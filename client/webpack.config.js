80143c50133cf3e37115063b71bb39e064d4b5e7
Eric Carles <carles.eric@wanadoo.fr>
2018-03-04 12:25:48 +0000

    configuration of the webpack.config.js

---
+++ client/webpack.config.js
@@ -0,0 +1,10 @@
+config = {
+  entry: __dirname + "/src/app.js",
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/build"
+  },
+  devtool: 'source-map'
+};
+
+module.exports = config;
