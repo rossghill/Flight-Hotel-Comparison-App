bdd45aedef2842441bb829dbeb1adde9d0002b10
Ross Hill <rosshill88@gmail.com>
2018-03-05 13:50:34 +0000

    Created searchHotel function in Amadeus API

--- server/api/amadeusAPI.js
+++ server/api/amadeusAPI.js
@@ -4,7 +4,9 @@ const ApiKey        = require('./apiKey');
 const AmadeusAPI = function(){
   this.amadeusApiKey        = new ApiKey().getAmadeusAPIKey();
   this.numberOfFlightResult = 5;
+  this.numberOfHotelResults = 20;
   this.onFlightsUpdate      = null;
+  this.onHotelsUpdate        = null;
 }
 
 
@@ -25,8 +27,19 @@ AmadeusAPI.prototype.searchFlights = function(airportFrom, airportTo, departureD
     }.bind(this));
 }
 
-AmadeusAPI.prototype.searchHotels = function(){
+AmadeusAPI.prototype.searchHotels = function(location, checkInDate, checkOutDate, radius){
     let url = `https://api.sandbox.amadeus.com/v1.2/hotels/search-airport?apikey=${this.amadeusApiKey}`;
+    url += `&location=${location}`;
+    url += `&checkInDate=${checkInDate}`;
+    url += `&checkOutDate=${checkOutDate}`;
+    url += `&radius=${radius}`;
+    url += `&numberOfResults=${this.numberOfHotelResults}`;
+
+    let request = new ServerRequest();
+    request.sendRequest(url, function(requestResponse){
+      this.onHotelsUpdate(JSON.parse(requestResponse));
+    }.bind(this));
+
 }
 
 
