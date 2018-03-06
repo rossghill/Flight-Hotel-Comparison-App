d9d25d2daccc4cc1a03d742f24624c2f433fa8ab
Mark Blanford <markytorture@hotmail.com>
2018-03-06 15:49:25 +0000

    Further tests on Hotel entity

--- client/src/entities/specs/hotelTest.js
+++ client/src/entities/specs/hotelTest.js
@@ -10,13 +10,13 @@ beforeEach(function(){
     "hotelName": "Hotel California",
     "price": 249.99,
     "currency": "USD",
-    "amenxities": ["POOL", "RESTAURANT", "WI-FI"],
+    "amenities": ["POOL", "RESTAURANT", "WI-FI"],
     "description": "You can check in any time you like, but you can never leave",
     "smallImage": "http://bxit.ly/2oLQcwQ",
     "bigImage": "http://bxit.ly/1T09dWo",
     "starRating": 5,
-    "latxitude": 666.666,
-    "longxitude": 777.777,
+    "latitude": 666.666,
+    "longitude": 777.777,
   }
   hotel1 = new Hotel(hotelDetails);
 });
@@ -24,43 +24,52 @@ beforeEach(function(){
 it('should have a name', function(){
   const actual = hotel1.hotelName;
   assert.strictEqual(actual, "Hotel California")
-})
+});
+
+it('should have a price', function(){
+  const actual = hotel1.price;
+  assert.strictEqual(actual, 249.99)
+});
+
+it('should have a currency', function(){
+  const actual = hotel1.currency
+  assert.strictEqual(actual, "USD");
+});
+
+it('should have amenities', function(){
+  const actual = hotel1.amenities;
+  assert.deepStrictEqual(actual, ["POOL", "RESTAURANT", "WI-FI"])
+});
+
+it('should have a description', function(){
+  const actual = hotel1.description;
+  assert.strictEqual(actual, "You can check in any time you like, but you can never leave")
+});
+
 xit('should have a', function(){
   const actual = hotel1.
   assert.strictEqual(actual, )
-})
+});
+
 xit('should have a', function(){
   const actual = hotel1.
   assert.strictEqual(actual, )
-})
+});
+
 xit('should have a', function(){
   const actual = hotel1.
   assert.strictEqual(actual, )
-})
+});
+
 xit('should have a', function(){
   const actual = hotel1.
   assert.strictEqual(actual, )
-})
+});
+
 xit('should have a', function(){
   const actual = hotel1.
   assert.strictEqual(actual, )
-})
-xit('should have a', function(){
-  const actual = hotel1.
-  assert.strictEqual(actual, )
-})
-xit('should have a', function(){
-  const actual = hotel1.
-  assert.strictEqual(actual, )
-})
-xit('should have a', function(){
-  const actual = hotel1.
-  assert.strictEqual(actual, )
-})
-xit('should have a', function(){
-  const actual = hotel1.
-  assert.strictEqual(actual, )
-})
+});
 
 
 
