4666fdfb4d13b2867d4fc7390194008445beec6c
Ross Hill <rosshill88@gmail.com>
2018-03-06 16:15:30 +0000

    Flight Package has outbound flight tested

---
+++ client/src/entities/specs/FlightPackageTest.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+const FlightPackage = require('../flightPackage');
+
+describe('Flight Packages', function(){
+
+let flightPackage;
+
+beforeEach(function() {
+  outboundFlights = {"origin": "LAX",
+                  "destination": "LON",
+                  "departureTime": "2018-03-16T12:50",
+                  "arrivalTime": "2018-03-16T17:10"};
+  inboundFlights = {"origin": "LON",
+                    "destination": "LAX",
+                    "departureDate": "2018-05-23T00:00",
+                    "arrivalTime": "2018-05-24T09:00"}
+  flightPackage = new FlightPackage(outboundFlights, inboundFlights, 900.00);
+});
+
+it('flight package has outbound flights', function(){
+  const actual = flightPackage.outboundFlights;
+  assert.deepStrictEqual(actual, {"origin": "LAX",
+                  "destination": "LON",
+                  "departureTime": "2018-03-16T12:50",
+                  "arrivalTime": "2018-03-16T17:10"})
+})
+
+
+});
