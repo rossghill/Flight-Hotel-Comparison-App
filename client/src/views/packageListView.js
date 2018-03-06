f748bc3794fa529fd8b9ea98bdacc69882d1932a
Eric Carles <carles.eric@wanadoo.fr>
2018-03-06 16:45:00 +0000

    Package list modified

--- client/src/views/packageListView.js
+++ client/src/views/packageListView.js
@@ -7,22 +7,13 @@ const PackageListView = function(){
 PackageListView.prototype.createPackageList = function(flightHotelPackages){
   const ul = document.getElementById('ul-packages');
 
-  console.log("createPackageList");
   for(flightHotelPackage of flightHotelPackages){
     const packageView = new PackageView();
     const div = packageView.createPackageView(flightHotelPackage);
-
-    console.log("flightHotelPackage");
-
     let li = document.createElement('li');
-    // li.appendChild(div);
-    li.innerText = flightHotelPackage;
+    li.appendChild(div);
     ul.appendChild(li);
-
-
   }
-
-
 }
 
 module.exports = PackageListView;
