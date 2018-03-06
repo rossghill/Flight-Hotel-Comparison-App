ca3026e5135855e9319cf8d7881d4fb65a148e4f
Ross Hill <rosshill88@gmail.com>
2018-03-06 16:23:46 +0000

    Flight Package has inbound flight total price tested

--- client/src/entities/specs/FlightPackageTest.js
+++ client/src/entities/specs/FlightPackageTest.js
@@ -23,7 +23,21 @@ it('flight package has outbound flights', function(){
                   "destination": "LON",
                   "departureTime": "2018-03-16T12:50",
                   "arrivalTime": "2018-03-16T17:10"})
+});
+
+it('flight package has inbound flights', function(){
+  const actual = flightPackage.inboundFlights;
+  assert.deepStrictEqual(actual, {"origin": "LON",
+                    "destination": "LAX",
+                    "departureDate": "2018-05-23T00:00",
+                    "arrivalTime": "2018-05-24T09:00"})
+});
+
+it('flight should have total price', function(){
+  const actual = flightPackage.totalPrice;
+  assert.strictEqual(actual, 900.00);
 })
+
 
 
 });
