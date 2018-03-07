5df0370567de343ebf7016bddf7eb04ed1c041fc
Ross Hill <rosshill88@gmail.com>
2018-03-07 10:29:23 +0000

    Test constructor created for FlightHotelPackages

---
+++ client/src/entities/specs/FlightHotelPackagesTest.js
@@ -0,0 +1,59 @@
+const assert = require('assert');
+const flightHotelPackage = require('../flightHotelPackage');
+const Hotel = require('../hotelEntity');
+
+
+describe('Flight Hotel Packages', function() {
+
+let fhps
+
+beforeEach(function() {
+
+  const outboundFlights = {"origin": "LAX",
+  "destination": "LON",
+  "departureTime": "2018-03-16T12:50",
+  "arrivalTime": "2018-03-16T17:10"};
+  const inboundFlights = {"origin": "LON",
+  "destination": "LAX",
+  "departureDate": "2018-05-23T00:00",
+  "arrivalTime": "2018-05-24T09:00"}
+  const flightPrice = 900;
+  const flightPackage = new FlightPackage(outboundFlights, inboundFlights, flightPrice);
+
+  const hotelDetails = {
+    "hotelName": "Hotel California",
+    "hotelPrice": 249.99,
+    "currency": "USD",
+    "amenities": ["POOL", "RESTAURANT", "WI-FI"],
+    "description": "You can check in any time you like, but you can never leave",
+    "smallImage": "http://bxit.ly/2oLQcwQ",
+    "bigImage": "http://bxit.ly/1T09dWo",
+    "starRating": 5,
+    "latitude": 666.666,
+    "longitude": 777.777,
+  }
+
+  const hotelDetails2 = {
+    "hotelName": "Cats Hotel",
+    "hotelPrice": 249.99,
+    "currency": "USD",
+    "amenities": ["RESTAURANT", "WI-FI"],
+    "description": "The only hotel owned and operated by cats",
+    "smallImage": "http://bxit.ly/2oLQcwQ",
+    "bigImage": "http://bxit.ly/1T09dWo",
+    "starRating": 5,
+    "latitude": 345.678,
+    "longitude": 123.456,
+  }
+
+  const hotel1 = new Hotel(hotelDetails);
+  const hotel2 = new Hotel(hotelDetails2);
+
+  const packagePrice = hotel1.hotelPrice + flightPackage.flightPrice;
+
+  fhp = new FlightHotelPackage(flightPackage, hotel1, packagePrice);
+
+  fhps = new FlightHotelPackages(flightHotelPackages, flightPackages)
+})
+
+})
