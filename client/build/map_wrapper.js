b1cd674c4fe3330fa40adde1ef4d7b58d1c26395
Ross Hill <rosshill88@gmail.com>
2018-03-09 15:18:46 +0000

    Split off functionality to create infor windows into map wrapper to use refactored code

--- client/build/map_wrapper.js
+++ client/build/map_wrapper.js
@@ -1,3 +1,6 @@
+const HotelView = require('./../src/views/hotelView');
+const HotelEntity = require('./../src/entities/hotelEntity');
+
 const MapWrapper = function (container, coords, zoom) {
   this.googleMap = new google.maps.Map(container, {
     center: coords,
@@ -13,14 +16,29 @@ MapWrapper.prototype.addMarker = function(coords) {
     position: coords,
     map: this.googleMap
   });
-  this.markers.push(marker);
+      this.markers.push(marker);
 }
 
-MapWrapper.prototype.addMarkerWithInfoWindow = function(coords) {
+MapWrapper.prototype.addMarkerWithInfoWindow = function(hotelEntity) {
+
+  let lat = hotelEntity.latitude;
+  let lng = hotelEntity.longitude;
+  let coords = {lat: lat, lng: lng};
+
   const marker = new google.maps.Marker({
     position: coords,
     map: this.googleMap
   });
+
+  let infowindow = new google.maps.InfoWindow();
+  let content = new HotelView().createHotelView(hotelEntity);
+
+  this.googleMap.event.addListener(marker,'click', (function(marker,content,infowindow){
+          return function() {
+             infowindow.setContent(content);
+             infowindow.open(this.googleMap,marker);
+          };
+      }));
   this.markers.push(marker);
 }
 
--- client/src/views/mapView.js
+++ client/src/views/mapView.js
@@ -14,41 +14,16 @@ MapView.prototype.populateMapWithHotels = function(travelPackages){
     let centerLng = travelPackage.hotel.longitude;
     let coords    = {lat:centerLat, lng:centerLng};
     this.map.addMarker(coords);
+
   }.bind(this));
 }
 
 
 MapView.prototype.removeAllMarkers = function(){
   this.map.removeAllMarker();
+}
 
-MapView.prototype.createGiantMap = function(flightHotelPackagesEntity){
-
-  let mapDiv    = document.getElementById("div-packages-map");
-
-  let centerLat = flightHotelPackagesEntity.destinationAirportLatitude;
-  let centerLng = flightHotelPackagesEntity.destinationAirportLongitude;
-
-  let coords = {lat:centerLat, lng:centerLng};
-  let giantMap = new MapWrapper(mapDiv, coords, 12);
-
-  flightHotelPackagesEntity.flightHotelPackages.forEach(function(flightHotelPackage){
-
-
-    let contentString = "Info goes here";
-
-     let centerLat = flightHotelPackage.hotel.latitude;
-     let centerLng = flightHotelPackage.hotel.longitude;
-     let coords = {lat:centerLat, lng:centerLng};
-     // giantMap.addMarker(coords);
-
-  var marker = new google.maps.Marker({
-          map: giantMap, title: hotel , position: coords
-        });
-        map.setCenter(marker.getPosition())
-
-
-  var infowindow = new google.maps.InfoWindow()
-
+//
 google.maps.event.addListener(marker,'click', (function(marker,content,infowindow){
         return function() {
            infowindow.setContent(contentString);
@@ -57,18 +32,18 @@ google.maps.event.addListener(marker,'click', (function(marker,content,infowindo
     })(marker,content,infowindow));
 
   });
-
-  // --------------------------
-
- MapView.prototype.createSmallMap = function(smallMapDiv, hotelEntity)
- {
-   let centerLat = hotelEntity.latitude;
-   let centerLng = hotelEntity.longitude;
-   let coords    = {lat:centerLat, lng:centerLng};
-   let smallMap  = new MapWrapper(smallMapDiv, coords, 12);
-   smallMap.addMarker(coords);
-
- }
+//
+//   // --------------------------
+//
+//  MapView.prototype.createSmallMap = function(smallMapDiv, hotelEntity)
+//  {
+//    let centerLat = hotelEntity.latitude;
+//    let centerLng = hotelEntity.longitude;
+//    let coords    = {lat:centerLat, lng:centerLng};
+//    let smallMap  = new MapWrapper(smallMapDiv, coords, 12);
+//    smallMap.addMarker(coords);
+//
+//  }
 
 
 module.exports = MapView;
